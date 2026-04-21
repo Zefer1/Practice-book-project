7897d34c189a2c105e1514fef7ed72b47e77cbac
José Fernandes <ze_932@hotmail.com>
2026-04-21 22:08:22 +0100

    feat: initial bookshelf API setup

---
+++ bookshelf-api/data.ts
@@ -0,0 +1,43 @@
+interface Book {
+    id: number,
+    title: string,
+    author: string,
+    status: "want to read" | "reading" | "finished...good book!" | "finished...trash! Dont read it"
+};
+
+let BOOKS: Book[] = [];
+
+let nextId = 1;
+
+export function addBook(title: string, author: string) {
+    const newBook = { id: nextId++, title, author, status: "want to read" as const};
+    BOOKS.push(newBook);
+    return newBook
+}
+
+export function getBook(id: number) {
+    const book = BOOKS.find(function(book) {
+        return book.id === id;
+    });
+    if(!book) {
+        throw new Error("Book not found! Please check for typos");
+    }
+    return book
+}
+
+export function getBooks() {
+    return BOOKS;
+}
+
+export function removeBook(id: number) {
+    BOOKS = BOOKS.filter(function(book){
+        return book.id !== id
+    })
+}
+
+export function updateBook(id:number, title: string, author: string, status: Book["status"]) {
+    const book = getBook(id)
+    book.title = title,
+    book.author = author,
+    book.status = status
+}
